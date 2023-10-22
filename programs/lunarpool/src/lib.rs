cfd0d308549224baac74fcf9508a10244638b0d8
dev.tronghieu <dev.ngtronghieu@gmail.com>
2023-10-22 19:11:05 +0700

    refactor: minimal setup

--- programs/lunarpool/src/lib.rs
+++ programs/lunarpool/src/lib.rs
@@ -6,10 +6,45 @@ declare_id!("BScWpTj5Jyp9EJJoLaKMGLEoRAxVPXqr24kvU28NPVeH");
 pub mod lunarpool {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn create_pool(
+        ctx: Context<CreatePool>,
+        mint_a: Pubkey,
+        mint_b: Pubkey,
+        fee: u8,
+        fee_account: Pubkey,
+    ) -> Result<()> {
+        let pool = &mut ctx.accounts.pool;
+        pool.owner = *ctx.accounts.owner.key;
+        pool.vault = *ctx.accounts.vault.key;
+        pool.mint_a = mint_a;
+        pool.mint_b = mint_b;
+        pool.fee = fee;
+        pool.fee_account = fee_account;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct CreatePool<'info> {
+    #[account(init, payer = owner, space = 8 + Pool::MAX_SIZE)]
+    pub pool: Account<'info, Pool>,
+    #[account(mut)]
+    pub owner: Signer<'info>,
+    #[account(mut)]
+    pub vault: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct Pool {
+    owner: Pubkey,       // 32
+    vault: Pubkey,       // 32
+    mint_a: Pubkey,      // 32
+    mint_b: Pubkey,      // 32
+    fee_account: Pubkey, // 32
+    fee: u8,             // 1
+}
+
+impl Pool {
+    pub const MAX_SIZE: usize = 32 + 32 + 32 + 32 + 32 + 1;
+}
--- tests/lunarpool.ts
+++ tests/lunarpool.ts
@@ -1,16 +1,141 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { Lunarpool } from "../target/types/lunarpool";
+import { expect } from "chai";
+import {
+    Account,
+    createMint,
+    getOrCreateAssociatedTokenAccount,
+    mintTo,
+} from "@solana/spl-token";
 
 describe("lunarpool", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+    const wallet = anchor.Wallet.local();
+    const provider = anchor.AnchorProvider.env();
+    anchor.setProvider(provider);
 
-  const program = anchor.workspace.Lunarpool as Program<Lunarpool>;
+    const program = anchor.workspace.Lunarpool as Program<Lunarpool>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
-  });
+    const poolKeypair = anchor.web3.Keypair.generate();
+    const ownerKeypair = anchor.web3.Keypair.generate();
+    const vaultKeypair = anchor.web3.Keypair.generate();
+    let mintA: anchor.web3.PublicKey;
+    let mintB: anchor.web3.PublicKey;
+    const fee = 5;
+    const feeAccountKeypair = anchor.web3.Keypair.generate();
+
+    const userKeypair = anchor.web3.Keypair.generate();
+    let userAtaA: Account;
+    let userAtaB: Account;
+
+    let latestBlockHash: {
+        blockhash: string;
+        lastValidBlockHeight: number;
+    };
+
+    const getAta = async (
+        mint: anchor.web3.PublicKey,
+        address: anchor.web3.PublicKey
+    ) => {
+        return await getOrCreateAssociatedTokenAccount(
+            provider.connection,
+            wallet.payer,
+            mint,
+            address
+        );
+    };
+
+    it("Airdrop", async () => {
+        latestBlockHash = await provider.connection.getLatestBlockhash();
+        let airdropSig = await provider.connection.requestAirdrop(
+            ownerKeypair.publicKey,
+            10_000_000_000
+        );
+
+        await provider.connection.confirmTransaction(
+            {
+                signature: airdropSig,
+                blockhash: latestBlockHash.blockhash,
+                lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
+            },
+            "confirmed"
+        );
+
+        airdropSig = await provider.connection.requestAirdrop(
+            userKeypair.publicKey,
+            10_000_000_000
+        );
+
+        await provider.connection.confirmTransaction(
+            {
+                signature: airdropSig,
+                blockhash: latestBlockHash.blockhash,
+                lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
+            },
+            "confirmed"
+        );
+    });
+
+    it("Mint", async () => {
+        mintA = await createMint(
+            provider.connection,
+            wallet.payer,
+            wallet.publicKey,
+            null,
+            6
+        );
+
+        mintB = await createMint(
+            provider.connection,
+            wallet.payer,
+            wallet.publicKey,
+            null,
+            6
+        );
+
+        userAtaA = await getAta(mintA, userKeypair.publicKey);
+
+        userAtaB = await getAta(mintB, userKeypair.publicKey);
+
+        await mintTo(
+            provider.connection,
+            wallet.payer,
+            mintA,
+            userAtaA.address,
+            wallet.payer,
+            20 * 10 ** 6
+        );
+
+        await mintTo(
+            provider.connection,
+            wallet.payer,
+            mintB,
+            userAtaB.address,
+            wallet.payer,
+            25 * 10 ** 6
+        );
+    });
+
+    it("Create Pool", async () => {
+        await program.methods
+            .createPool(mintA, mintB, fee, feeAccountKeypair.publicKey)
+            .accounts({
+                pool: poolKeypair.publicKey,
+                owner: ownerKeypair.publicKey,
+                vault: vaultKeypair.publicKey,
+            })
+            .signers([poolKeypair, ownerKeypair, vaultKeypair])
+            .rpc();
+
+        const pool = await program.account.pool.fetch(poolKeypair.publicKey);
+        expect(pool.owner.toString()).to.equal(
+            ownerKeypair.publicKey.toString()
+        );
+        expect(pool.mintA.toString()).to.equal(mintA.toString());
+        expect(pool.mintB.toString()).to.equal(mintB.toString());
+        expect(pool.fee).to.equal(fee);
+        expect(pool.feeAccount.toString()).to.equal(
+            feeAccountKeypair.publicKey.toString()
+        );
+    });
 });
